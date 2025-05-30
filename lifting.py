69141b170c59845796ada22bfae81305d4b83f8a
Benedikt Waibel <49001513+Alkalem@users.noreply.github.com>
2025-05-30 13:45:02 +0200

    Added lifting of simple move and add instructions.

--- lifting.py
+++ lifting.py
@@ -1,9 +1,35 @@
-from binaryninja.lowlevelil import LowLevelILFunction
+from binaryninja.lowlevelil import LowLevelILFunction, ExpressionIndex
 from binaryninja import log_warn
 
 from .constants import ARCH_SIZE, HALF_SIZE
-from .instruction import Instruction
+from .instruction import Instruction, Operand, IntegerOperand, RegisterOperand
 
+
+#TODO: lift conditional execution
+
+def to_il(operand:Operand, il:LowLevelILFunction) -> ExpressionIndex:
+    if isinstance(operand, IntegerOperand):
+        return il.const(HALF_SIZE, operand.get_value())
+    elif isinstance(operand, RegisterOperand):
+        return il.reg(ARCH_SIZE, operand.get_value())
+    else:
+        raise NotImplementedError(f'lifting of {type(operand)}')
+
+def lift_add(instr:Instruction, il:LowLevelILFunction):
+    #TODO: handle all input variants and determine sizes based on ops
+    src1 = to_il(instr.ops[0], il)
+    src2 = to_il(instr.ops[1], il)
+    dst = instr.ops[2].get_value()
+    il.append(il.set_reg(ARCH_SIZE, dst, il.add(
+        ARCH_SIZE, src1, src2
+    )))
+
+def lift_addk(instr:Instruction, il:LowLevelILFunction):
+    imm = instr.ops[0].get_value()
+    reg = instr.ops[1].get_value()
+    il.append(il.set_reg(ARCH_SIZE, reg, il.add(
+        ARCH_SIZE, il.const(HALF_SIZE, imm), il.reg(ARCH_SIZE, reg)
+    )))
 
 def lift_mvk(instr: Instruction, il: LowLevelILFunction):
     imm = instr.ops[0].get_value()
@@ -20,12 +46,27 @@ def lift_nop(instr: Instruction, il: LowLevelILFunction):
     il.append(il.nop())
 
 
+def lift_branch(instr: Instruction, il: LowLevelILFunction):
+    il.append(il.call(il.reg(ARCH_SIZE, instr.ops[0].get_value())))
+    return
+
+def lift_mv(instr: Instruction, il: LowLevelILFunction):
+    il.append(il.set_reg(ARCH_SIZE, instr.ops[1].get_value(),
+            il.reg(ARCH_SIZE, instr.ops[0].get_value())))
+
+
 HANDLERS_BY_MNEMONIC = {
+    'ADD': lift_add,
+    'ADDK': lift_addk,
+    'B': lift_branch,
     'MVK': lift_mvk,
     'MVKL': lift_mvk,
     'MVKH': lift_mvkh,
     'MVKLH': lift_mvkh,
-    'NOP': lift_nop
+    'NOP': lift_nop,
+
+    # Capstone pseudo-instruction
+    'MV': lift_mv
 }
 
 
