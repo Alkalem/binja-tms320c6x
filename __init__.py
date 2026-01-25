3b058be3db2218fbb3a2e95a979fddd20be355a8
Benedikt Waibel <49001513+Alkalem@users.noreply.github.com>
2026-01-25 16:29:41 +0100

    Changed license to match disassembler library.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,19 @@
+# Copyright 2025-2026 Benedikt Waibel
+# 
+# This file is part of the binary ninja tms320c6x architecture plugin.
+# 
+# This plugin is free software: 
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
 import binaryninja as _bn
 
 from .arch import TMS320C67x, C67Call, TMS320C6x
